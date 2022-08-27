3ba9e2f8693d2f74d474cfbb57140865d8ca9e63
linjiasen63 <linjiasen63@gmail.com>
2022-08-27 23:00:24 +0800

    手动备份（2022-08-27 23:00:24）

--- 02.内容管理/1000.日常生活/1200.财务状况/1210.日常账单/1211.其它内容/analyse.go
+++ 02.内容管理/1000.日常生活/1200.财务状况/1210.日常账单/1211.其它内容/analyse.go
@@ -17,7 +17,7 @@ type BillRecord struct {
 }
 
 func main() {
-	filepath := `/Users/linjiasen/personal/life-management/02.内容管理/1000.日常生活/1200.财务状况/1210.日常账单/2022/06月.md`
+	filepath := `/Users/linjiasen/personal/life-management/02.内容管理/1000.日常生活/1200.财务状况/1210.日常账单/2022/08月.md`
 
 	file, err := os.Open(filepath)
 	if err != nil {
